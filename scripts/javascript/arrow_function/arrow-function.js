a92a2f63fd623d183ad043bd012993982b48ecf5
Fauzan <fauzanlubis23@gmail.com>
2019-10-19 13:04:03 +0800

    update example

--- scripts/javascript/arrow_function/arrow-function.js
+++ scripts/javascript/arrow_function/arrow-function.js
@@ -13,3 +13,17 @@ function funcName(params) {
 let funcName = (params) => params + 3
 funcName(3);
 // 6
+
+
+//contoh mapping array dengan arrow function
+
+let kucings = [
+    'Siam',
+    'Anggora',
+    'Garong',
+    'Persia'
+  ];
+  
+  console.log(kucings.map(kucing => kucing.length));
+  // Array [4, 7, 6, 6]
+  
